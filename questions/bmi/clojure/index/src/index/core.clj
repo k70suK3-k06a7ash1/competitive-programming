4532f0c0603139b80d1bf4f4479b8ffcba72a122
2814109 <2814109k@gmail.com>
2024-11-02 06:31:04 +0900

    chore

--- questions/bmi/clojure/index/src/index/core.clj
+++ questions/bmi/clojure/index/src/index/core.clj
@@ -1,6 +1,13 @@
 (ns index.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn calc [height weight]
+  (let [unit (/ height 100)
+        denominator (* unit unit)]
+        ;; double型からfloat型へキャスト
+    (Math/round (float (/ weight denominator)))))
+
+(defn rate [bmi]
+  (cond
+    (< bmi 20) "A"
+    (and (> bmi 20) (< bmi 25)) "B"
+    (>= bmi 25) "C"))
--- questions/bmi/clojure/index/test/index/core_test.clj
+++ questions/bmi/clojure/index/test/index/core_test.clj
@@ -1,7 +1,26 @@
+;; (ns index.core-test
+;;   (:require [clojure.test :refer :all]
+;;             [index.core :refer :all]))
+
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
 (ns index.core-test
   (:require [clojure.test :refer :all]
             [index.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-calc
+  (testing "BMI calculation"
+    (is (= 22 (calc 170 65)))
+    (is (= 25 (calc 180 80)))
+    ))
+
+(deftest test-rate
+  (testing "BMI rating"
+    (is (= "A" (rate 19)))
+    (is (= "B" (rate 22)))
+    (is (= "C" (rate 26)))))
+
+(defn -main []
+  (run-tests 'index-test))
