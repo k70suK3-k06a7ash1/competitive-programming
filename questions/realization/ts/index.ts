e608ec5ace3ff7ad2e548c8aea3252aa01874468
2814109 <2814109k@gmail.com>
2024-11-05 05:30:40 +0900

    chore

--- questions/realization/ts/index.ts
+++ questions/realization/ts/index.ts
@@ -9,5 +9,7 @@ const N: number = 4// Number.parseInt(input[0].trim());
 const A: number[] = [3,10,7,3]// input[1].split(" ").map((line) => Number.parseInt(line.trim())) // .slice(1).map((line) => line.trim());
 const P: number[] = [31,41, 59, 26, 53, 58, 97, 93, 23, 84]// input[2].split(" ").map((line) => Number.parseInt(line.trim())) //.slice(2).map((line) => line.trim());
 
-const result = [...Array(N)].map((_, i) =>  P[A[i]-1]).reduce((sum, element) =>  (sum + element), 0);
+// const result = [...Array(N)].map((_, i) =>  P[A[i]-1]).reduce((sum, element) =>  (sum + element), 0);
+const result = Array.from({ length: N }, (_, i) => P[A[i] - 1]).reduce((sum, element) => sum + element, 0);
+
 console.log(result)
