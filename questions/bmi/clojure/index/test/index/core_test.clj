6fe581b06090fb3ea1b366ef3ad69e6b46e97aab
2814109 <2814109k@gmail.com>
2024-11-04 22:56:18 +0900

    chore

--- questions/bmi/clojure/index/test/index/core_test.clj
+++ questions/bmi/clojure/index/test/index/core_test.clj
@@ -1,6 +1,3 @@
-;; (ns index.core-test
-
-
 (ns index.core-test
   (:require [clojure.test :refer :all]
             [index.core :refer :all]))
@@ -8,8 +5,7 @@
 (deftest test-calc
   (testing "BMI calculation"
     (is (= 22 (calc 170 65)))
-    (is (= 25 (calc 180 80)))
-    ))
+    (is (= 25 (calc 180 80)))))
 
 (deftest test-rate
   (testing "BMI rating"
